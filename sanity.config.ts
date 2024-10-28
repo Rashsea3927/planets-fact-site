71d032265a48f539390880c1ee312c4e0b59a56b
Rashsea3927 <kaji.dev3927@gmail.com>
2024-10-28 21:15:35 +0900

    Change projectId

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,14 +3,13 @@ import { structureTool } from 'sanity/structure';
 import { visionTool } from '@sanity/vision';
 import { colorInput } from '@sanity/color-input';
 import { schemaTypes } from './schemaTypes';
-import { dataset, projectId } from './env';
 
 export default defineConfig({
   name: 'default',
   title: 'planets-fact-site',
 
-  projectId: projectId,
-  dataset: dataset,
+  projectId: 'ddmndcen',
+  dataset: 'production',
   basePath: '/studio',
   plugins: [structureTool(), visionTool(), colorInput()],
 
