2e51ec66aba7e7d82afcd08dc7fe21aba74b0c94
Rashsea3927 <kaji.dev3927@gmail.com>
2024-10-28 21:28:03 +0900

    set variable

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,13 +3,14 @@ import { structureTool } from 'sanity/structure';
 import { visionTool } from '@sanity/vision';
 import { colorInput } from '@sanity/color-input';
 import { schemaTypes } from './schemaTypes';
+import { dataset, projectId } from './env';
 
 export default defineConfig({
   name: 'default',
   title: 'planets-fact-site',
 
-  projectId: 'ddmndcen',
-  dataset: 'production',
+  projectId: projectId,
+  dataset: dataset,
   basePath: '/studio',
   plugins: [structureTool(), visionTool(), colorInput()],
 
